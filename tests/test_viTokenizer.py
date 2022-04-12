363e92689c08bd8842bce954422cb1b5915bde1d
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2022-04-12 21:13:55 +0700

    Update test_viTokenizer.py

--- tests/test_viTokenizer.py
+++ tests/test_viTokenizer.py
@@ -3,11 +3,32 @@ from pyvi.ViTokenizer import ViTokenizer
 
 class TestViTokenizer(TestCase):
     def test_sylabelize(self):
-        text = "Thủ tướng: Chỉ số thị trường chứng khoán Việt Nam trong top 3 thế giới"
-        actual = ViTokenizer.sylabelize(text)
-        expected = ['Thủ', 'tướng', ':', 'Chỉ', 'số', 'thị', 'trường', 'chứng', 'khoán', 'Việt', 'Nam', 'trong',
+        cases_ = [
+            [
+                "Thủ tướng: Chỉ số thị trường chứng khoán Việt Nam trong top 3 thế giới",
+                ['Thủ', 'tướng', ':', 'Chỉ', 'số', 'thị', 'trường', 'chứng', 'khoán', 'Việt', 'Nam', 'trong',
                         'top', '3', 'thế', 'giới']
-        self.assertEqual(actual, expected)
+            ],
+            [
+                "FLC đề nghị hủy toàn bộ giao dịch cổ phiếu đột biến ngày 1/4 nếu phát hiện vi phạm",
+                ["FLC", "đề", "nghị", "hủy", "toàn", "bộ", "giao", "dịch", "cổ", "phiếu", "đột", "biến",
+                 "ngày", "1/4", "nếu", "phát", "hiện", "vi", "phạm"]
+            ],
+            [
+                "Công ty Cổ phần Tập đoàn FLC (HoSE: FLC) vừa có văn bản gửi Bộ Tài chính, Uỷ ban Chứng" \
+                "khoán Nhà nước, Sở Giao dịch chứng khoán TP. HCM và Trung tâm Lưu ký Chứng khoán Việt" \
+                "Nam đề nghị kiểm tra giao dịch đột biến trong ngày đối với cổ phiếu FLC.",
+                ['Công', 'ty', 'Cổ', 'phần', 'Tập', 'đoàn', 'FLC', '(', 'HoSE', ':', 'FLC', ')', 'vừa', 'có',
+                 'văn', 'bản', 'gửi', 'Bộ', 'Tài', 'chính', ',', 'Uỷ', 'ban', 'Chứng', 'khoán', 'Nhà', 'nước',
+                 ',', 'Sở', 'Giao', 'dịch', 'chứng', 'khoán', 'TP.', 'HCM', 'và', 'Trung', 'tâm', 'Lưu', 'ký',
+                 'Chứng', 'khoán', 'Việt', 'Nam', 'đề', 'nghị', 'kiểm', 'tra', 'giao', 'dịch', 'đột', 'biến',
+                 'trong', 'ngày', 'đối', 'với', 'cổ', 'phiếu', 'FLC', '.']
+            ]
+        ]
+        for item in cases_:
+            actual = ViTokenizer.sylabelize(item[0])
+            expected = item[1]
+            self.assertEqual(actual, expected)
 
     def test_sylabelize_2(self):
         texts = open("tokenize_sets.txt").readlines()
